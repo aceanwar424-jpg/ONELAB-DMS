db14a17e95298f25f104b0ba8fe7c54da36cd570
aceanwar424-jpg <aceanwar424@gmail.com>
2026-06-02 19:19:26 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sop-wiki/', // Change to your GitHub repo name
+  base: '/onelab-dms-production-921d.up.railway.app/', // Change to your GitHub repo name
   server: {
     proxy: {
       '/api': {
