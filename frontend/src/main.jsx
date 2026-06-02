a3a7a4a45c3600fcadcbc7744ea74102f419ee7b
aceanwar424-jpg <aceanwar424@gmail.com>
2026-06-02 19:20:00 +0700

    Change BrowserRouter basename for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sop-wiki">
+    <BrowserRouter basename="/onelab-dms-production-921d.up.railway.app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
